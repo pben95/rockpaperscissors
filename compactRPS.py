3760728b7d599ea06898d8f7232e023e973647e3
pben95 <pben95@users.noreply.github.com>
2018-02-14 19:41:44 -0500

    Add files via upload

--- compactRPS.py
+++ compactRPS.py
@@ -1,6 +1,8 @@
 import random
 states, rps = {"rs" : "Win", "sp" : "Win", "pr" : "Win", "sr" : "Lose", "ps" : "Lose", "rp" : "Lose", "rr" : "Tie", "pp" : "Tie", "ss" : "Tie"}, ["r", "p", "s"]
+score = {"Win" : 0, "Lose" : 0, "Tie" : 0}
 while True:
-    inp = input('Rock paper scissors (type r, p, or s)').lower()
+    inp, opp = input('Rock paper scissors (type r, p, or s)').lower(), random.randint(0, 2)
     if inp in rps:
-        print(states[inp+rps[random.randint(0,2)]])
+        score[states[inp + rps[opp]]] += 1
+        print(states[inp+rps[opp]], score)
